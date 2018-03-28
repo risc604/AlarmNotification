bb276d754ff3ceeaf091ae6a0ca69c939888b657
tomcat <>
2018-03-28 23:15:04 +0800

    add resource code.

--- app/src/main/java/com/demo/tomcat/alarmnotification/AlarmReceiver.java
+++ app/src/main/java/com/demo/tomcat/alarmnotification/AlarmReceiver.java
@@ -4,12 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class AlarmReceiver extends BroadcastReceiver {
+public class AlarmReceiver extends BroadcastReceiver
+{
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent)
+    {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+
     }
+
+
 }
+
--- app/src/main/java/com/demo/tomcat/alarmnotification/MainActivity.java
+++ app/src/main/java/com/demo/tomcat/alarmnotification/MainActivity.java
@@ -1,13 +1,56 @@
 package com.demo.tomcat.alarmnotification;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    TextView    tvMsgBox;
+    Button      btnSwitch;
+
+    boolean     btnStatus = true;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initControl();
     }
+
+
+    @SuppressLint("SetTextI18n")
+    public void ButtonOnClick(View view)
+    {
+        if (btnStatus)
+        {
+            btnSwitch.setText("Stop");
+            btnStatus = false;
+        }
+        else
+        {
+            btnSwitch.setText("Start");
+            btnStatus = true;
+        }
+    }
+
+
+    //-------------------- User function ------------------------//
+    private void initView()
+    {
+        tvMsgBox = findViewById(R.id.textviewMsg);
+        btnSwitch = findViewById(R.id.buttonSwitch);
+    }
+
+    private void initControl()
+    {
+
+    }
+
 }
